4f896c4d082bfc3e52d27bd2e692cea03c1cfae3
pcquynh <pcquynh@mun.ca>
2022-06-13 22:22:50 -0230

    Add update route for question

--- back-end/src/server.mjs
+++ back-end/src/server.mjs
@@ -5,100 +5,123 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, "/build")));
 app.use(express.json());
 
-const client = new MongoClient('mongodb://localhost:27017');
+const client = new MongoClient("mongodb://localhost:27017");
 
 //Rebuild database with new JSON data.
 //Working. Data entered must be correct, no error checking currently.
-app.post('/api/init/:apiKey', async (req, res) => {
-    const { apiKey } = req.params;
-    const triviaData = req.body;
-    if (apiKey === "5f767fa2-1f5a-4e45-ba11-4d7001a6744e") {
-        console.log("Initializing trivia database with JSON data.");
-        try{
-            await client.connect();
-            const db = client.db("triviaDatabase");
-            await db.collection('trivia').deleteMany( {} );
-            await db.collection('trivia').insertMany( triviaData );
-            const triviaInfo = await db.collection('trivia').find({}).toArray();
-            res.status(200).json(triviaInfo);
-            client.close();
-        }catch(error){
-            res.sendStatus(500);
-        }
+app.post("/api/init/:apiKey", async (req, res) => {
+  const { apiKey } = req.params;
+  const triviaData = req.body;
+  if (apiKey === "5f767fa2-1f5a-4e45-ba11-4d7001a6744e") {
+    console.log("Initializing trivia database with JSON data.");
+    try {
+      await client.connect();
+      const db = client.db("triviaDatabase");
+      await db.collection("trivia").deleteMany({});
+      await db.collection("trivia").insertMany(triviaData);
+      const triviaInfo = await db.collection("trivia").find({}).toArray();
+      res.status(200).json(triviaInfo);
+      client.close();
+    } catch (error) {
+      res.sendStatus(500);
     }
-})
+  }
+});
 
 //Retrieve question data.
 //TODO: Pass a parameter in the request to specify which question to return and return that specific question.
-app.get('/api/question', async (req, res) => {
-    try{
-        await client.connect();
-        const db = client.db("triviaDatabase");
-        const questionInfo = await db.collection('trivia').find({}).project({"correctAnswer":0,"_id":0}).toArray();
-        res.status(200).json(questionInfo);
-        client.close();
-    }catch(error){
-        res.sendStatus(500);
-    }
+app.get("/api/question", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("triviaDatabase");
+    const questionInfo = await db
+      .collection("trivia")
+      .find({})
+      .project({ correctAnswer: 0 })
+      .toArray();
+    res.status(200).json(questionInfo);
+    client.close();
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 //Check answer and send back correct/incorrect.
 //Working in postman, test in program!
-app.post('/api/checkanswer', async (req, res) => {
-    const choice = req.body.choice;
-    const question = req.body.question;
+app.post("/api/checkanswer", async (req, res) => {
+  const choice = req.body.choice;
+  const question = req.body.question;
 
-    try{
-        await client.connect();
-        const db = client.db("triviaDatabase");
-        const questionInfo = await db.collection('trivia').findOne({"question": question});
-        if (questionInfo.correctAnswer === choice){
-            res.status(200).send("correct");
-        }else{
-            res.status(200).send("incorrect");
-        }
-        client.close();
-    }catch(error){
-        res.sendStatus(500);
+  try {
+    await client.connect();
+    const db = client.db("triviaDatabase");
+    const questionInfo = await db
+      .collection("trivia")
+      .findOne({ question: question });
+    if (questionInfo.correctAnswer === choice) {
+      res.status(200).send("correct");
+    } else {
+      res.status(200).send("incorrect");
     }
+    client.close();
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 //Add one single question to the database.
 //Working. No error checking.
-app.post('/api/add', async (req, res) => {
-    const question = req.body.question;
-    const choiceA = req.body.choiceA;
-    const choiceB = req.body.choiceB;
-    const choiceC = req.body.choiceC;
-    const correctAnswer = req.body.correctAnswer;
+app.post("/api/add", async (req, res) => {
+  const question = req.body.question;
+  const choiceA = req.body.choiceA;
+  const choiceB = req.body.choiceB;
+  const choiceC = req.body.choiceC;
+  const correctAnswer = req.body.correctAnswer;
 
-    try{
-        await client.connect();
-        const db = client.db("triviaDatabase");
-        await db.collection('trivia').insertOne( {
-            "question": question,
-            "choiceA": choiceA,
-            "choiceB": choiceB,
-            "choiceC": choiceC,
-            "correctAnswer": correctAnswer
-        });
-        const questionInfo = await db.collection('trivia').find({}).toArray();
-        res.status(200).json(questionInfo);
-        client.close();
-    }catch(error){
-        res.sendStatus(500);
-    }
+  try {
+    await client.connect();
+    const db = client.db("triviaDatabase");
+    await db.collection("trivia").insertOne({
+      question: question,
+      choiceA: choiceA,
+      choiceB: choiceB,
+      choiceC: choiceC,
+      correctAnswer: correctAnswer,
+    });
+    const questionInfo = await db.collection("trivia").find({}).toArray();
+    res.status(200).json(questionInfo);
+    client.close();
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
+// Test in postman, have cors error in application
+app.put("/api/update/:id", async (req, res) => {
+  const { usedDate } = req.body;
+  try {
+    await client.connect();
+    const db = client.db("triviaDatabase");
+    const updatedQuestion = await db
+      .collection("trivia")
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        { $set: { usedDate: usedDate } }
+      );
+    res.status(200).json(updatedQuestion);
+    client.close();
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 app.listen(8000, () => console.log("Listening on port 8000."));
--- front-end/src/components/Question.js
+++ front-end/src/components/Question.js
@@ -2,54 +2,54 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col } from "react-bootstrap";
 
-function Question({ question, checkAnswer = f => f}) {
-    return (
-      <>
-        <Row className="justify-content-center">
-          <Col className="col-7 border border-dark">
-            <h5>{question.question}</h5>
-          </Col>
-        </Row>
-        <br></br>
-        <br></br>
-        <Row className="justify-content-center">
-          <a
-            href="#"
-            class="col-7 btn btn-dark btn-lg active"
-            role="button"
-            aria-pressed="true"
-            onClick={() => checkAnswer("A")}
-          >
-            A. {question.choiceA}
-          </a>
-        </Row>
-        <br></br>
-        <Row className="justify-content-center">
-          <a
-            href="#"
-            class="col-7 btn btn-dark btn-lg active"
-            role="button"
-            aria-pressed="true"
-            onClick={() => checkAnswer("B")}
-          >
-            B. {question.choiceB}
-          </a>
-        </Row>
-        <br></br>
-        <Row className="justify-content-center">
-          <a
-            href="#"
-            class="col-7 btn btn-dark btn-lg active"
-            role="button"
-            aria-pressed="true"
-            onClick={() => checkAnswer("C")}
-          >
-            C. {question.choiceC}
-          </a>
-        </Row>
-        <br></br>
-      </>
-    );
+function Question({ question, checkAnswer = (f) => f }) {
+  return (
+    <>
+      <Row className="justify-content-center">
+        <Col className="col-7 border border-dark">
+          <h5>{question.question}</h5>
+        </Col>
+      </Row>
+      <br></br>
+      <br></br>
+      <Row className="justify-content-center">
+        <a
+          href="#"
+          class="col-7 btn btn-dark btn-lg active"
+          role="button"
+          aria-pressed="true"
+          onClick={() => checkAnswer("A")}
+        >
+          A. {question.choiceA}
+        </a>
+      </Row>
+      <br></br>
+      <Row className="justify-content-center">
+        <a
+          href="#"
+          class="col-7 btn btn-dark btn-lg active"
+          role="button"
+          aria-pressed="true"
+          onClick={() => checkAnswer("B")}
+        >
+          B. {question.choiceB}
+        </a>
+      </Row>
+      <br></br>
+      <Row className="justify-content-center">
+        <a
+          href="#"
+          class="col-7 btn btn-dark btn-lg active"
+          role="button"
+          aria-pressed="true"
+          onClick={() => checkAnswer("C")}
+        >
+          C. {question.choiceC}
+        </a>
+      </Row>
+      <br></br>
+    </>
+  );
 }
 
 export default Question;
