a89a6ada516052176d0a4a2e1346cb03cf5032b4
Riley Burke <rileyburke0@hotmail.com>
2022-06-15 10:10:28 -0230

    Fixed Results re-rendering and added Total Time

--- front-end/src/components/Results.js
+++ front-end/src/components/Results.js
@@ -2,7 +2,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col } from "react-bootstrap";
 
-function Results({ score }) {
+function Results({ score, time }) {
+  console.log(time);
+  let totalTime = (Date.now() - time)/1000;
   return (
     <>
       <Row className="justify-content-center">
@@ -11,7 +13,7 @@ function Results({ score }) {
           <h5>Your Score: {score}</h5>
         </Col>
         <Col className="col-3 text-end">
-          <h5>Total time taken: 0:00s</h5>
+          <h5>Total time taken: {totalTime}s</h5>
           <h5>Next SpeedTriv</h5>
         </Col>
       </Row>
