9f24bf69ac267dd8e79e3d9b536b3e011080d886
Riley Burke <rileyburke0@hotmail.com>
2022-06-21 21:31:43 -0230

    Fixed Countdown Timer

--- back-end/src/server.mjs
+++ back-end/src/server.mjs
@@ -103,3 +103,17 @@ app.get("*", (req, res) => {
 });
 
 app.listen(8000, () => console.log("Listening on port 8000."));
+
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync('/etc/letsencrypt/live/speedtriv.ca/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/speedtriv.ca/fullchain.pem'),
+// }, app);
+
+// httpServer.listen(80, () => {
+//     console.log('HTTP Server running on port 80');
+// });
+
+// httpsServer.listen(443, () => {
+//     console.log('HTTPS Server running on port 443');
+// });
